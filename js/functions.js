5cae968431a63ada43c50db5c99e9c03029b3ca3
gineth22 <getrillosp@ufpso.edu.co>
2023-10-27 14:17:12 -0500

    cargando archivos

---
+++ js/functions.js
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console
+console.log(name);
+// body
+document.write(name + "###Gineth Trillos")
+document.getElementById("box_one").innerHTML
+document.getElementById("box_two").innerText
+// librerias
+//Swal.fire(
+//    'The Internet?',
+ //   'That thing is still around?',
+//    'question'
+//  )
+//Swal.fire({
+//     icon:'error',
+//     showConfirmButton: false,
+//     tittle: name,
+//     text: name,
+//     footer: "footer: "+name,
+//     background: 'green',
+//     position: 'top-start'
+// });
+
+//######tipos de datos
+//numericos
+
+var number_one = 20;
+var number_two = 5.5
+//texto
+var class_type = "electiva tecnica 1";
+//booleanos
+var boolean = true; //flase
+//array - vectores
+var array_num = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+var array_mix = [1, "a",2, "c"];
+var array_mu1 = [
+    {name: "gineth", last_name:"trillos", age: "19"},
+    {name: "gineth", last_name:"trillos", age: "19"},
+    {name: "gineth", last_name:"trillos", age: "19"},
+    {name: "gineth", last_name:"trillos", age: "19"}
+]
+
+//###### operadores basicos 
+//suma
+var suma = number_one + number_two;
+console.log("suma="suma);
+//resta
+var resta = number_one - number_two;
+console.log("resta="resta);
+//multiplicacion
+var multi = number_one * number_two;
+console.log("multiplicacion="multi);
+//division
+var divi = number_one / number_two;
+console.log("division="divi.toFixed(2));
+//modulo
+var modulo= number_one % number_two;
+console.log("modulo="modulo)
